851ca3530e3e3c9ec3278e10e3d6e81a4e93eefa
Anshika <ansrkghamirpur@gmail.com>
2024-08-28 16:40:10 +0530

    chages

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -13,27 +13,43 @@ class LoginPage extends StatelessWidget {
             "assets/image/login_po.png",
             fit: BoxFit.cover,
             height: 200,
-            ),
-
-            const SizedBox(
-              height: 10.0,
-            ),
-          const Text(
-            "Welcome!",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold
-            ),
           ),
           const SizedBox(
-              height: 10.0,
-            ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: "Username",
-              labelText: "Username"
-            ),
-          )
+            height: 10.0,
+          ),
+          const Text(
+            "Welcome!",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Post Office!");
+                    },
+                    child: const Text("Login"),
+                  ),
+                ],
+              ))
         ],
       ),
     );
